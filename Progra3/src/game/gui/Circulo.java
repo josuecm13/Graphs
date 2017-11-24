f8d51379d0d5057bc1be730df51bb954a0766fe0
Josué Canales <josuecanales0@gmail.com>
2017-11-23 22:22:26 -0600

    virus

--- Progra3/src/game/gui/Circulo.java
+++ Progra3/src/game/gui/Circulo.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gui;
+
+package game.gui;
 
 import java.awt.Canvas;
 import java.awt.Color;
--- Progra3/src/game/gui/SwingPaint.java
+++ Progra3/src/game/gui/SwingPaint.java
@@ -1,4 +1,4 @@
-package gui;
+package game.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
